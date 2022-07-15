6db0510d927d116fb91b88a06b3943028a753894
caruccithomas <caruccithomasignacio@gmail.com>
2022-07-15 01:33:06 -0300

    committed

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import Product from './ProductItem'
 import { popular_products } from '../data'
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,4 +1,4 @@
-import React, {Fragment, useState} from "react"
+import React, { useState} from "react"
 import styled from "styled-components"
 import Announcement from "../components/Announcement"
 import OtherAnnouncement from "../components/OtherAnnouncement"
