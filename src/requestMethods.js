e78f3080305f5ee6eac090006cf63f7344618a25
caruccithomas <caruccithomasignacio@gmail.com>
2023-01-09 03:44:08 -0300

    fixed api url

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "https://bronx-ecommerce-api.onrender.com"
+const BASE_URL = "https://bronx-ecommerce-api.onrender.com/e-commerce_api/"
 // const BASE_URL = "http://localhost:5000/e-commerce_api"
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user
